2d7bb416a78674c49c3b0b4ab88ce46ee88c2ce0
Sheron Silva <sheronvitorino@hotmail.com>
2022-12-20 21:07:33 -0400

    second

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-    $('#cpf').mask('000.000.000-00')
-    $('#telefone').mask('(00) 00000-0000')
-    $('#cep').mask('00000-000')
+    $('#cpf').mask('00000000000')
+    $('#telefone').mask('00000000000')
+    $('#cep').mask('00000000')
 
     $('form').validate ({
         rules: {
